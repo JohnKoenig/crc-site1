118981d9c11b2d2fa712f8685198536e450f2e14
JohnKoenig <JohnKoenig@users.noreply.github.com>
2021-09-07 14:21:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-fetch('https://34vp8z8p64.execute-api.us-east-2.amazonaws.com/default/johnkoenig_ninja_counter/')
-   .then(response => response.json())
-   .then(characters => showCharacters(characters.results));
+fetch("https://34vp8z8p64.execute-api.us-east-2.amazonaws.com/default/johnkoenig_ninja_counter")
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("NETWORK RESPONSE ERROR");
+    }
+  })
+  .then(data => {
+    console.log(data);
+    displayCocktail(data)
+  })
+  .catch((error) => console.error("FETCH ERROR:", error));
 
-   showCharacters = characters => {
-    const charactersDiv = document.querySelector('#rick-and-morty-characters');
-
-  }
+  function displayCocktail(data) {
+    const cocktail = data.drinks[0];
+    const cocktailDiv = document.getElementById("siteCount");
+    const cocktailName = cocktail.strDrink;
+    const heading = document.createElement("h1");
+    heading.innerHTML = cocktailName;
+    cocktailDiv.appendChild(heading);
+  }   
