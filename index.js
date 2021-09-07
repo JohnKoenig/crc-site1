b2d04fb70decb6d0541b5537a3f4c2ba8c9bf00b
JohnKoenig <JohnKoenig@users.noreply.github.com>
2021-09-07 13:57:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-fetch('https://rickandmortyapi.com/api/character/')
+fetch('https://34vp8z8p64.execute-api.us-east-2.amazonaws.com/default/johnkoenig_ninja_counter/')
    .then(response => response.json())
    .then(characters => showCharacters(characters.results));
 
