6025ec974607f6cda3c02a254ff22a832695d141
JohnKoenig <JohnKoenig@users.noreply.github.com>
2021-09-07 13:51:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 fetch('https://34vp8z8p64.execute-api.us-east-2.amazonaws.com/default/johnkoenig_ninja_counter')
    .then(response => response.json())
    .then(characters => showCharacters(characters.results));
+
+   showCharacters = characters => {
+    const charactersDiv = document.querySelector('#rick-and-morty-characters');
+    characters.forEach(character => {
+      const characterElement = document.createElement('p');
+      characterElement.innerText = `Character Name: ${character.name}`;
+      charactersDiv.append(characterElement);
+    });
+  }
